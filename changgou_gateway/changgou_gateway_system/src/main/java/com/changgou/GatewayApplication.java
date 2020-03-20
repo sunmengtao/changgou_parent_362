8a5727b9a33fa9fbbdcaaaa8c7d4dac1881768d4
孙孟涛 <hello@itcast.cn>
2020-03-20 21:09:51 +0800

    feat:新增网管基于redis的限流实现

--- changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/GatewayApplication.java
+++ changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/GatewayApplication.java
@@ -2,7 +2,11 @@ package com.changgou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,5 +14,15 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,args);
+    }
+
+    @Bean
+    public KeyResolver ipKeyResolver(){
+        return new KeyResolver(){
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                return Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+            }
+        };
     }
 }
