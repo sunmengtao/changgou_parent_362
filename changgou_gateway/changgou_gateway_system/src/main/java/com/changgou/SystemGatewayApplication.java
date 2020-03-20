2e9fba76968eaa325b2388074f3cc2580c5d1a23
wujintao <wujintao@itcast.cn>
2020-03-20 11:25:03 +0800

    feat:新增网关基于redis的限流实现

--- changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/SystemGatewayApplication.java
+++ changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/SystemGatewayApplication.java
@@ -2,7 +2,11 @@ package com.changgou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,5 +14,17 @@ public class SystemGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SystemGatewayApplication.class,args);
+    }
+
+    @Bean
+    public KeyResolver ipKeyResolver(){
+
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                //设置限流的维度，根据什么限流：根据用户IP来限流
+                return Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+            }
+        };
     }
 }
