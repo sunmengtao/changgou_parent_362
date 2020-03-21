3288b9b5e9aeb93e4aa5fdf4ee9898805201275e
孙孟涛 <hello@itcast.cn>
2020-03-21 19:38:58 +0800

    feat:    新增jwt测试用例,测试jwt设置过jwt时间    新增jwt测试用例,测试jwt载荷里设置自定义属性

--- changgou_service/changgou_service_system/src/test/java/com/changgou/system/JWTTest.java
+++ changgou_service/changgou_service_system/src/test/java/com/changgou/system/JWTTest.java
@@ -29,16 +29,49 @@ public class JWTTest {
         System.out.println("TOKEN值为：" + token);
     }
 
+    @Test
+    public void testCreateJwtExpire(){
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, "itheima")
+                .setId(UUID.randomUUID().toString()) //JWT的唯一ID
+                .setSubject("黑马程序员") //JWT的主题
+                .setIssuedAt(new Date())//JWT的生成时间
+                .setExpiration(new Date(System.currentTimeMillis()+60*60*1000))
+                .compact();
+        System.out.println("TOKEN值为：" + token);
+    }
+
+    @Test
+    public void testCreateJwtCustom(){
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, "itheima")
+                .setId(UUID.randomUUID().toString()) //JWT的唯一ID
+                .setSubject("黑马程序员") //JWT的主题
+                .setIssuedAt(new Date())//JWT的生成时间
+                .setExpiration(new Date(System.currentTimeMillis()+60*60*1000))
+                .claim("username","zhangsan")
+                .claim("age",20)
+                .claim("address","北京昌平")
+                .compact();
+        System.out.println("TOKEN值为：" + token);
+    }
+
 
     /**
      * 解析JWT
      */
     @Test
     public void parseJwt(){
-        String jwt = "eyJhbGciOiJIUzI1NiJ9.1eyJqdGkiOiJlODA4NTU2ZS02NDA1LTQyOGQtOTY1My1iZjE4OTc0ZjFiZGMiLCJzdWIiOiLpu5HpqaznqIvluo_lkZgiLCJpYXQiOjE1ODQ2ODcwNTh9.ts4lehpSWl27IMJC3IYNCVGbuy41DZN_0VZ1j6lIA8U";
+        String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiMzYzYWYxZC03OWU5LTQ2YjUtYjRmYy02Mzc3NzliZmU0OGYiLCJzdWIiOiLpu5HpqaznqIvluo_lkZgiLCJpYXQiOjE1ODQ3OTA0MDQsImV4cCI6MTU4NDc5NDAwNCwidXNlcm5hbWUiOiJ6aGFuZ3NhbiIsImFnZSI6MjAsImFkZHJlc3MiOiLljJfkuqzmmIzlubMifQ.y88a-n3Qgf_COsTDnJ0EcQPSr_HbgJW2YzJ2MIuZMXw";
         Jws<Claims> parse = Jwts.parser().setSigningKey("itheima").parseClaimsJws(jwt);
         System.out.println("解析头：" + parse.getHeader());
-        System.out.println("解析头：" + parse.getBody());
-        System.out.println("解析头：" + parse.getSignature());
+        Claims body = parse.getBody();
+        System.out.println("解析载荷：" + parse.getBody());
+        System.out.println("ID" + body.getId());
+        System.out.println("SUB" + body.getSubject());
+        System.out.println("IAT" + body.getIssuedAt());
+        System.out.println("EXP" + body.getExpiration());
+        System.out.println("username:"+body.get("username"));
+        System.out.println("age:"+body.get("age"));
+        System.out.println("address:"+body.get("address"));
+        System.out.println("解析签名：" + parse.getSignature());
     }
 }
