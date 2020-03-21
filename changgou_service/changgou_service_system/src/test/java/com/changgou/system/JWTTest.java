cc94b563be5d8ec149fd91c07f4fc5ccbb50f750
wujintao <wujintao@itcast.cn>
2020-03-21 09:39:21 +0800

    feat: 1、新增JWT测试用例，测试JWT设置过JWT时间 2、新增JWT测试用例，测试JWT载荷里设置自定义属性

--- changgou_service/changgou_service_system/src/test/java/com/changgou/system/JWTTest.java
+++ changgou_service/changgou_service_system/src/test/java/com/changgou/system/JWTTest.java
@@ -20,11 +20,38 @@ public class JWTTest {
      * eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlODA4NTU2ZS02NDA1LTQyOGQtOTY1My1iZjE4OTc0ZjFiZGMiLCJzdWIiOiLpu5HpqaznqIvluo_lkZgiLCJpYXQiOjE1ODQ2ODcwNTh9.ts4lehpSWl27IMJC3IYNCVGbuy41DZN_0VZ1j6lIA8U
      */
     @Test
-    public void testCreateJwt(){
+    public void testCreateJwt() {
         String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, "itheima")
                 .setId(UUID.randomUUID().toString()) //JWT的唯一ID
                 .setSubject("黑马程序员") //JWT的主题
                 .setIssuedAt(new Date())//JWT的生成时间
+                .compact();
+        System.out.println("TOKEN值为：" + token);
+    }
+
+
+    @Test
+    public void testCreateJwtExpire(){
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, "itheima")
+                .setId(UUID.randomUUID().toString()) //JWT的唯一ID
+                .setSubject("黑马程序员") //JWT的主题
+                .setIssuedAt(new Date())//JWT的生成时间
+                .setExpiration(new Date(System.currentTimeMillis()+60*60*1000))//JWT的过期时间，比如设置过期时间1个小时
+                .compact();
+        System.out.println("TOKEN值为：" + token);
+    }
+
+
+    @Test
+    public void testCreateJwtCustom(){
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, "itheima")
+                .setId(UUID.randomUUID().toString()) //JWT的唯一ID
+                .setSubject("黑马程序员") //系统属性：JWT的主题
+                .setIssuedAt(new Date())//系统属性：JWT的生成时间
+                .setExpiration(new Date(System.currentTimeMillis()+60*60*1000))//系统属性：JWT的过期时间，比如设置过期时间1个小时
+                .claim("username", "zhangsan")  //自定义属性: 用户名
+                .claim("age", 20) //自定义属性: 用户名
+                .claim("address", "北京昌平") //自定义属性：地址
                 .compact();
         System.out.println("TOKEN值为：" + token);
     }
@@ -35,10 +62,18 @@ public class JWTTest {
      */
     @Test
     public void parseJwt(){
-        String jwt = "eyJhbGciOiJIUzI1NiJ9.1eyJqdGkiOiJlODA4NTU2ZS02NDA1LTQyOGQtOTY1My1iZjE4OTc0ZjFiZGMiLCJzdWIiOiLpu5HpqaznqIvluo_lkZgiLCJpYXQiOjE1ODQ2ODcwNTh9.ts4lehpSWl27IMJC3IYNCVGbuy41DZN_0VZ1j6lIA8U";
+        String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmZTlhMTdjNi1mNTQyLTRiYTItOTY4ZC05ZGE1NGNiZmIwMGEiLCJzdWIiOiLpu5HpqaznqIvluo_lkZgiLCJpYXQiOjE1ODQ3NTQ1MjgsImV4cCI6MTU4NDc1ODEyOCwidXNlcm5hbWUiOiJ6aGFuZ3NhbiIsImFnZSI6MjAsImFkZHJlc3MiOiLljJfkuqzmmIzlubMifQ.y9moKMB1Hsn8MzkySSR7YkdrPGHrHSQsg_tVBgA8eYM";
         Jws<Claims> parse = Jwts.parser().setSigningKey("itheima").parseClaimsJws(jwt);
         System.out.println("解析头：" + parse.getHeader());
-        System.out.println("解析头：" + parse.getBody());
-        System.out.println("解析头：" + parse.getSignature());
+        Claims body = parse.getBody();
+        System.out.println("解析载荷：" + parse.getBody());
+        System.out.println("ID："+ body.getId());
+        System.out.println("SUB："+ body.getSubject());
+        System.out.println("IAT："+ body.getIssuedAt());
+        System.out.println("EXP："+ body.getExpiration());
+        System.out.println("username："+ body.get("username"));
+        System.out.println("age："+ body.get("age"));
+        System.out.println("address："+ body.get("address"));
+        System.out.println("解析签名：" + parse.getSignature());
     }
 }
