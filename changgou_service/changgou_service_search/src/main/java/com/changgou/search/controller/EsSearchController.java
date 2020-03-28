af7305e16cbe4730a784947018c9477b110713d7
孙孟涛 <hello@itcast.cn>
2020-03-28 20:38:11 +0800

    feat: 1.实现搜索结果列表页的渲染-回显商品数据,搜索条件,URL 2.实现搜索结果列表页的渲染-URL排除特殊字段(排序字段和pageNum) 3.实现搜索结果列表页的渲染-URL中特殊字符+的处理

--- changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/EsSearchController.java
+++ changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/EsSearchController.java
@@ -1,8 +1,11 @@
 package com.changgou.search.controller;
 
 
+import com.changgou.entity.Page;
 import com.changgou.search.service.EsSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/search")
 public class EsSearchController {
 
@@ -18,9 +21,39 @@ public class EsSearchController {
     @Autowired
     private EsSearchService esSearchService;
 
+
     @GetMapping("/list")
-    public Map search(@RequestParam Map<String,String> searchMap){
+    public String search(@RequestParam Map<String,String> searchMap, Model model){
         Map result = esSearchService.search(searchMap);
-        return result;
+        model.addAttribute("result",result);
+        model.addAttribute("searchMap",searchMap);
+
+        Long total = Long.valueOf(String.valueOf(result.get("total")));
+        int pageNum = Integer.valueOf(String.valueOf(result.get("pageNum")));
+        Page page = new Page(total, pageNum, Page.pageSize);
+        model.addAttribute("page",page);
+
+        StringBuffer url = new StringBuffer();
+        url.append("/search/list");
+        if (searchMap!=null && searchMap.size()>0){
+            url.append("?");
+            int flag = 0;
+            for (String key : searchMap.keySet()) {
+                if (flag>0){
+                    url.append("&");
+                }
+                if (!"sortField".equalsIgnoreCase(key) && !"sortRule".equalsIgnoreCase(key) && !"pageNum".equalsIgnoreCase(key)){
+
+                    String val = searchMap.get(key);
+                    val = val.replace("+","%2B");
+
+                    url.append(key + "=" + val);
+                    flag++;
+                }
+
+            }
+        }
+        model.addAttribute("url",url.toString());
+        return "search";
     }
 }
