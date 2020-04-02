3ce252ee1933466de3195d35b57dc893fb48df12
wujintao <wujintao@itcast.cn>
2020-04-02 12:03:34 +0800

    feat: 1、页面渲染微服务通过FeignInterceptor解决微服务调用无法传递Authorization请求头的问题 2、订单微服务通过TokenDecode获取登录用户名

--- changgou_service/changgou_service_order/src/main/java/com/changgou/order/controller/CartController.java
+++ changgou_service/changgou_service_order/src/main/java/com/changgou/order/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.changgou.order.controller;
 
+import com.changgou.config.TokenDecode;
 import com.changgou.entity.Result;
 import com.changgou.entity.StatusCode;
 import com.changgou.order.service.CartService;
@@ -18,30 +19,33 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    @Autowired
+    private TokenDecode tokenDecode;
+
     @GetMapping("/add")
     public Result add(@RequestParam("skuId") String skuId,@RequestParam("num") Integer num){
-        String  usernmae = "zhangsan";
-        cartService.add(usernmae, skuId, num);
+        String  username = tokenDecode.getUserInfo().get("username");
+        cartService.add(username, skuId, num);
         return new Result(true, StatusCode.OK, "新增购物车成功");
     }
 
     @GetMapping("/list")
     public Result list(){
-        String  usernmae = "zhangsan";
-        Map map = cartService.list(usernmae);
+        String  username = tokenDecode.getUserInfo().get("username");
+        Map map = cartService.list(username);
         return new Result(true, StatusCode.OK, "查询成功", map);
     }
 
     @GetMapping("/updateChecked")
     public Result updateChecked(@RequestParam("skuId") String skuId,@RequestParam("checked") Boolean checked){
-        String username = "zhangsan";
+        String  username = tokenDecode.getUserInfo().get("username");
         cartService.updateChecked(username, skuId, checked );
         return new Result(true, StatusCode.OK, "勾选或取消成功");
     }
 
     @GetMapping("/delete")
     public Result delete(@RequestParam("skuId") String skuId){
-        String username = "zhangsan";
+        String  username = tokenDecode.getUserInfo().get("username");
         cartService.delete(username, skuId);
         return new Result(true, StatusCode.OK, "删除成功");
     }
